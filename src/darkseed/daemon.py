831a4a5cfbcbe525bd4a3a4ffa9912c26ef14cbd
virtu <virtu@cryptic.to>
2024-07-08 10:03:55 +0200

    Add configuration options, use logging

--- src/darkseed/daemon.py
+++ src/darkseed/daemon.py
@@ -1,13 +1,15 @@
 """Darkseed daemon that listens for DNS requests and commands."""
 
 import copy
+import logging as log
 import random
 import socketserver
 import threading
 import time
-from pathlib import Path
 
 from dnslib import AAAA, QTYPE, RR, TXT, A, DNSRecord
+
+from darkseed.config import get_config
 
 # Generate dummy records until real data is read from crawler outputs
 SeedRecords = []
@@ -17,9 +19,9 @@ for i in range(20):
     SeedRecords.append((QTYPE.TXT, TXT(f"Test data {i}")))
 
 
-def dns_handler(data, addr):
+def dns_handler(data, addr) -> bytearray:
     request = DNSRecord.parse(data)
-    print(f"Received DNS request from {addr}")
+    log.debug("Received DNS request from %s", addr)
 
     # if TimeUp():
     #   create new selection
@@ -33,11 +35,13 @@ def dns_handler(data, addr):
     size_limit = 512
     while True:
         if not pool:
-            print("No more records to return.")
-            return
+            log.warning("Ran out of reachable nodes during reply creation.")
+            if num_recs == 0:
+                log.warning("Returning empty reply")
+            return reply.pack()
 
         rtype, rdata = random.choice(pool)
-        # log.debug print(f"attempting to add rtype={rtype}, rdata={rdata}")
+        # log.debug("attempting to add rtype=%s, rdata=%s", rtype, rdata)
         # todos:
         # - avoid dups
         # - inbue with domain knowledge (e.g., if CJDNS, I2P, and TOR records exist,
@@ -49,17 +53,23 @@ def dns_handler(data, addr):
 
         if len(reply_new.pack()) > size_limit:
             # exit early. room for opt: try another record
-            print(f"new_size={len(reply_new.pack())}B exceeds limit={size_limit}B")
+            log.debug(
+                "new_size=%dB exceeds limit=%dB", len(reply_new.pack()), size_limit
+            )
             break
 
         num_recs += 1
-        print(
-            f"Added record (data={rrec.rdata}, type={rrec.rtype}, number={num_recs}, "
-            f"prev_size={len(reply.pack())}B, new_size={len(reply.pack())}B)"
+        log.debug(
+            "Added record (data=%s, type=%s, number=%d, prev_size=%dB, new_size=%dB)",
+            rrec.rdata,
+            rrec.rtype,
+            num_recs,
+            len(reply.pack()),
+            len(reply_new.pack()),
         )
         reply = reply_new
 
-    print(f"Done creating reply (size={len(reply.pack())}B, records={num_recs}")
+    log.info("Created reply (size=%dB, records=%d)", len(reply.pack()), num_recs)
     return reply.pack()
 
 
@@ -69,15 +79,14 @@ class DNSRequestHandler(socketserver.BaseRequestHandler):
         socket = self.request[1]
         response = dns_handler(data, self.client_address)
         reply_size = len(response)
-        print(f"Sending DNS reply (size={reply_size})...", end="")
         socket.sendto(response, self.client_address)
-        print("done.")
+        log.debug("Sent DNS reply (size=%d, to=%s)", reply_size, self.client_address)
 
 
 class CommandServerHandler(socketserver.BaseRequestHandler):
     def handle(self):
         data = self.request.recv(1024).strip().decode("utf-8")
-        print(f"Command received: {data}")
+        log.debug("Command received: %s", data)
         response = ""
         if data.lower() == "status":
             response = "Server running. Uptime: 100 minutes."
@@ -102,12 +111,32 @@ def start_command_server():
 # def list_files_in_directory(directory):
 #     while True:
 #         files = os.listdir(directory)
-#         print(f"Files in {directory}: {files}")
+#         log.debug(f"Files in {directory}: {files}")
 #         time.sleep(600)  # Sleep for 10 minutes (600 seconds)
 #
 
 
+def init():
+    """
+    Handle initialization.
+
+    First, check requirements, then parse command-line arguments and create
+    settings object. Next, sanity-check the settings and initialize the logger.
+    """
+
+
 def main():
+    """Parse command-line arguments, set up logging, and run darkseed daemon."""
+
+    conf = get_config()
+    log.basicConfig(
+        level=conf.log_level,
+        format="%(asctime)s | %(levelname)-8s | %(message)s",
+        datefmt="%Y-%m-%dT%H:%M:%SZ",
+    )
+    log.Formatter.converter = time.gmtime
+    log.info("Using configuration: %s", conf)
+
     dns_thread = threading.Thread(target=start_dns_server)
     command_thread = threading.Thread(target=start_command_server)
     # nodes_thread = threading.Thread(
@@ -115,9 +144,9 @@ def main():
     # )
 
     dns_thread.start()
-    print("Started DNS thread.")
+    log.info("Started DNS thread.")
     command_thread.start()
-    print("Started command thread.")
+    log.info("Started command thread.")
 
 
 if __name__ == "__main__":
