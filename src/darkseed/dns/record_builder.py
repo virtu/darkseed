53cbdc801e35c539516b3b5c1040c91eddb52f60
virtu <virtu@cryptic.to>
2024-07-15 14:44:28 +0200

    Replace custom @declarator

--- src/darkseed/dns/record_builder.py
+++ src/darkseed/dns/record_builder.py
@@ -13,26 +13,17 @@ from dns.rdtypes.IN.AAAA import AAAA
 from darkseed.node import Address
 
 
-def address_type_checker(func):
-    """Decorator to check if the address has a valid type before proceeding."""
-
-    def wrapper(self, address, *args, **kwargs):
-        if not (address.ipv4 or address.ipv6 or address.cjdns):
-            raise ValueError("Unsupported address type!")
-        return func(self, address, *args, **kwargs)
-
-    return wrapper
-
-
 class RecordBuilder:
     """Class for building DNS resource records."""
 
-    @address_type_checker
     @staticmethod
     def build_record(
         address: Address, domain: str = "seed.21.ninja.", ttl: int = 60
     ) -> dns.rrset.RRset:
         """Build a DNS record for a node using the specified encoding."""
+        if not (address.ipv4 or address.ipv6 or address.cjdns):
+            raise ValueError(f"Unsupported address type: {address.net_type}")
+
         rdata = RecordBuilder.get_rdata(address)
         record = dns.rrset.from_rdata(domain, ttl, rdata)
 
@@ -44,11 +35,11 @@ class RecordBuilder:
         )
         return record
 
-    @address_type_checker
     @staticmethod
     def get_rdata(address: Address) -> dns.rdata.Rdata:
         """Get DNS resource record data."""
         if address.ipv4:
             return A(IN, A_TYPE, address.address)
-        # ipv6 or cjdns
-        return AAAA(IN, AAAA_TYPE, address.address)
+        if address.ipv6 or address.cjdns:
+            return AAAA(IN, AAAA_TYPE, address.address)
+        raise ValueError(f"Unsupported address type: {address.net_type}")
